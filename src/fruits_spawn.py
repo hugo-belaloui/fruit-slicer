40aeb76a2b7c5ce4c8c1ee4f49f115b16d772510
yanissaouri <yaniss.aouri@laplateforme.io>
2026-01-27 15:46:52 +0100

    feat: added random fruit showing on screen

--- src/fruits_spawn.py
+++ src/fruits_spawn.py
@@ -1,13 +1,26 @@
 import pygame
-import window
+import random
+import time
 
 
+FRUITS_NAMES = ["apple", "mango", "kiwi", "bomb", "ice"]
+SCALE = 0.5
 
-apple = pygame.image.load("fruit-slicer/assets/apple.png")
-w = apple.get_width()
-h = apple .get_height()
-apple = pygame.transform.scale(apple ,(w * 0.5, h *0.5))
+def load_fruits():
+    fruits = []
+
+    for name in FRUITS_NAMES:
+        img = pygame.image.load(f"fruit-slicer/assets/{name}.png")
+        w, h = img.get_width(), img.get_height()
+        img = pygame.transform.scale(img, (int(w * SCALE), int(h * SCALE)))
+        fruits.append(img)
+
+    return fruits
+
+
+fruits = load_fruits()
+
+fruit = random.choice(fruits)
 
 def draw_fruit(screen):
-    screen.blit(apple,(50,40))
-
+        screen.blit(fruit, (00, 150))
