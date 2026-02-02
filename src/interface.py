fec7d9dfb39fa29d6da7b252a9739c5eb2dcb141
Alya Annabi <237869273+Alyaannabi7@users.noreply.github.com>
2026-02-02 15:25:12 +0100

    change of comments to English

--- src/interface.py
+++ src/interface.py
@@ -2,11 +2,11 @@ import pygame
 
 pygame.init()
 
-#taille de la fenêtre
+#window size
 window_width, window_height = 1000, 600
 screen = pygame.display.set_mode((window_width, window_height))
 
-#Charger les images
+#Load the images
 bg_image = pygame.image.load("image/fondnumber1.jpg")
 
 buttonplay_img = pygame.image.load("image/buttonfondplay.png").convert_alpha() 
@@ -17,61 +17,61 @@ buttonparam_img = pygame.image.load("image/parametre.png").convert_alpha()
 
 button_back_img = pygame.image.load("image/retour.png").convert_alpha()
 
-#Images son
+#Images music
 son_on_img = pygame.image.load("image/son.png").convert_alpha()
 son_off_img = pygame.image.load("image/coupeson.png").convert_alpha()
 
-#le fond pour les paramètres
+#background for the settings
 bg_params = pygame.image.load("image/fondparameter.png") 
 bg_params_scaled = pygame.transform.scale(bg_params, (window_width, window_height))
 
 
-#Couleurs
+#Colors
 white = (255, 255, 255)
 
-#Musique de fond  (fond1)
+#Background music (background1)
 pygame.mixer.music.load("song./Sketchbook 2024-10-26.ogg")
 pygame.mixer.music.play(-1)
 
-#Zone du bouton (Position et Taille)
+#Button Area (Position and Size)
 button_rect = pygame.Rect(356, 300, 310, 180)
-#redimensionne l'image PNG pour qu'elle fasse exactement la taille du bouton
+#resize the PNG image so that it is exactly the size of the button
 button_img = pygame.transform.scale(buttonplay_img, (button_rect.width, button_rect.height))
 
 
-#Zone du bouton (Position et Taille)
+#Button Area (Position and Size)
 exit_rect = pygame.Rect(356, 393, 310, 180)
-#redimensionne l'image PNG pour qu'elle fasse exactement la taille du bouton
+#resize the PNG image so that it is exactly the size of the button
 exit_img = pygame.transform.scale(buttonexit_img, (exit_rect.width, exit_rect.height))
 
 
-#BOUTON PARAMETRE (FOND 2)
-parameter_rect = pygame.Rect(830, 20, 200, 100) #en haut à droite
+#BUTON PARAMETER(background 2)
+parameter_rect = pygame.Rect(830, 20, 200, 100) #at the top right
 parameter_img = pygame.transform.scale(buttonparam_img, (parameter_rect.width, parameter_rect.height))
 
 
 
-#BOUTON RETOUR (PARAMETRES)
+#BACK BUTTON (SETTINGS)
 back_rect = pygame.Rect(20, 20, 200, 100) #en haut à gauche
 back_img = pygame.transform.scale(button_back_img, (back_rect.width, back_rect.height))
 
-#BOUTON SON (PARAMETRES)
+#BUTON music (SETTINGS)
 mute_rect = pygame.Rect(420, 250, 180, 100) # Au milieu de l'écran paramètres son
 son_on_scaled = pygame.transform.scale(son_on_img, (mute_rect.width, mute_rect.height))
 son_off_scaled = pygame.transform.scale(son_off_img, (mute_rect.width, mute_rect.height))
 
 
-#Police pour le texte
+#Font for the text
 font = pygame.font.Font("LuckyRookie Regular 400.ttf", 60) 
 
-play_pressed = False#indique si on a cliqué sur play ou pas
+play_pressed = False#indicates whether play was clicked or not
 
-params_open = False#servira à savoir si on affiche les paramètres ou pas
+params_open = False#will be used to determine whether to display the settings or not
 
-is_muted = False # NOUVEAU : pour savoir si le son est coupé
+is_muted = False #NEW: to know if the sound is muted
 
 
-#Boucle principale
+#Main loop
 running = True
 while running:
     for event in pygame.event.get():
@@ -79,7 +79,7 @@ while running:
             running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            # Si on est sur le premier menu
+            #If we are on the first menu
             if not play_pressed:
                 if button_rect.collidepoint(event.pos):
                     play_pressed = True
@@ -92,44 +92,44 @@ while running:
 
 
 
-            #    CLICS MENU PARAMÈTRES (FOND 3) 
+            #    CLICKS MENU SETTINGS (BACKGROUND 3)
 
-            elif params_open:#Si on est dans les paramètres
+            elif params_open:#If we are in the settings
                 if back_rect.collidepoint(event.pos):
-                    params_open = False # On ferme les paramètres pour revenir au Fond 2
+                    params_open = False # We close the settings to return to Background 2
 
-                # NOUVEAU : Clic sur le bouton MUTE
+                # NEW: Click the MUTE button
                 if mute_rect.collidepoint(event.pos):
-                    is_muted = not is_muted # On inverse l'état (ON devient OFF et vice-versa)
+                    is_muted = not is_muted #We reverse the state (ON becomes OFF and vice-versa)
                     
                     if is_muted:
-                        pygame.mixer.music.set_volume(0) # Coupe le volume
+                        pygame.mixer.music.set_volume(0) # Turn down the volume
                     else:
-                        pygame.mixer.music.set_volume(1) # Remet le volume à fond
+                        pygame.mixer.music.set_volume(1) # Turn the volume up all the way
 
                 if exit_rect.collidepoint(event.pos):
                     running = False
 
 
-            #  CLICS JEU (FOND 2)  
+            #CLICK GAME (BACKGROUND 2)
             
             else:
                 if parameter_rect.collidepoint(event.pos):
                     params_open = True
 
 
-    if not play_pressed:#Affichage du menu principal tant que play n'est pas cliqué
-        #Affichage du fond
+    if not play_pressed:#Display the main menu until play is clicked
+#Display the background
         bg_scaled = pygame.transform.scale(bg_image, (window_width, window_height))
         screen.blit(bg_scaled, (0, 0))
         
-        #image png du bouton texte play
+        #PNG image of the text play button
         screen.blit(button_img, (button_rect.x, button_rect.y))
-        text = font.render("PLAY", True, white) # couleur du texte
+        text = font.render("PLAY", True, white) # color of the text
         text_rect = text.get_rect(center=button_rect.center) # centrer le texte
         screen.blit(text, text_rect) # afficher le texte sur le bouton
 
-        #image png du bouton texte play
+        #PNG image of the text play button
         screen.blit(exit_img, (exit_rect.x, exit_rect.y))
         text_exit = font.render("EXIT", True, white)
         screen.blit(text_exit, text_exit.get_rect(center=exit_rect.center))
@@ -137,12 +137,12 @@ while running:
 
 
     elif params_open:
-        #Affichage du fond de paramètres
+        #Display of settings background
         screen.blit(bg_params_scaled, (0, 0))
-        #affiche le boutton retour
+        #display back button
         screen.blit(back_img, (back_rect.x, back_rect.y))
 
-        if is_muted:# affiche l'icône son coupé ou non
+        if is_muted:#display the correct image based on the sound state
             screen.blit(son_off_scaled, (mute_rect.x, mute_rect.y))
         else:
             screen.blit(son_on_scaled, (mute_rect.x, mute_rect.y))
@@ -152,12 +152,12 @@ while running:
         screen.blit(text_exit, text_exit.get_rect(center=exit_rect.center))
 
     else:
-        #Fond 2 uniquement après le clic play
+        #ackground for the game
         bg_game = pygame.image.load("image/fond2.jpg")
         bg_game_scaled = pygame.transform.scale(bg_game, (window_width, window_height))
         screen.blit(bg_game_scaled, (0, 0))
 
-        #affiche le boutton paramètre
+        #display parameter button
         screen.blit(parameter_img, (parameter_rect.x, parameter_rect.y))
         
         
